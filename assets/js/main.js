113ded03fb4faca096141e54dddcd39b32b723ff
AlanS2405 <alan.dario.solis@gmail.com>
2023-04-30 01:12:37 -0300

    fin del primer video

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+async function getProducts() {
+    try {
+        const data = await fetch (
+            "https://ecommercebackend.fundamentos-29.repl.co/"
+            );
+
+            const res = await data.json();
+
+            return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function main() {
+    const res = await getProducts();
+
+    console.log(res);
+}
+
+main();
